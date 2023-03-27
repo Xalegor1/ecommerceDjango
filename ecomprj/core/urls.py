1f47c9b0be70b426805d125f9b43acfa479ad741
Xalegor <Argus1672@gmail.com>
2023-03-27 12:59:29 +0500

    context_processors

--- ecomprj/core/urls.py
+++ ecomprj/core/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 
 urlpatterns = [
+    # Homepage
     path('', views.index, name='index'),
     path('products/', views.product_list, name='products-list'),
+    path('prod-detail/<pid>/', views.prod_detail_view, name='prod-detail'),
+
+    # Category
+    path('category/', views.category_list_view, name="category-list"),
+    path('category/<cid>/', views.prod_lst_categ_view, name="category-product-list"),
 
 ]
 
--- ecomprj/core/views.py
+++ ecomprj/core/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Product, Category, Vendor, ProductImages, CartOrder, CartOrderItems, ProductReview, WishList, Address
 
 
 def index(request):
     products = Product.objects.all().order_by("-id")
+    categories = Category.objects.all()
+    
 
     context = {
-        "products": products
+        "products": products,
+        "categories": categories
+
     }
     return render(request, 'index.html', context)
 
@@ -18,3 +22,36 @@ def product_list(request):
         "products": products
     }
     return render(request, 'products-list.html', context)
+
+
+
+def category_list_view(request):
+    categories = Category.objects.all()
+    context = {
+        "categories": categories
+    }
+    return render(request, "category-list.html", context)
+
+
+def prod_lst_categ_view(request, cid):
+    category = Category.objects.get(cid=cid)
+    products = Product.objects.filter(product_status="published", category=category)
+    mama = Category.objects.all()
+    count = Category.objects.filter(cid=cid).count()
+
+    context = {
+        "category": category,
+        "products": products,
+        "categories": mama,
+        'count': count
+    }
+    return render(request, "category-prd-lst.html", context)
+
+
+def prod_detail_view(request, pid):
+    # product = Product.objects.get(pid=pid)
+    product = get_object_or_404(Product, pid=pid)
+    context = {
+        "product": product
+    }
+    return render(request, "product-detail.html", context)
